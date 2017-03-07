40136d206a39b1bd46234dd5cc57413b5b8ff95a
gsaindras <gsaindras@undiksha.ac.id>
2017-03-07 08:51:11 +0800

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Home Page</title>
+
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}" >
+    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+</head>
+<body>
+
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+</body>
+</html>
